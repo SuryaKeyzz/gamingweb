3df6e512aa38b44320bec4bbb178f0eedc529f0b
SuryaKeyzz <gilbertsuryali1@gmail.com>
2024-08-21 05:36:54 +0800

    mengedit index html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
-  darkMode: 'class',
+  content: ["./index.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
       padding: "16px",
     },
     extend: {
+      backgroundImage: {
+        good: "url('/img/background.jpg')",
+        evil: "url('/img/background2.jpg')",
+      },
       colors: {
-        primary : "#2C7873",
+        primary: "#2C7873",
         dark: "#0f172a",
         tosca: "#5b247a",
         blues: "#1bcedf",
         secondary: "#64748b",
-      }
+      },
     },
   },
   plugins: [],
-}
-
+};
